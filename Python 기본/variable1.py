de7e01421046024d30c1b417933121ba9a862f46
lived1024 <lived1024@gmail.com>
2025-03-30 00:18:19 +0900

    숫자형 및 불린형 실습습

---
+++ Python 기본/variable1.py
@@ -0,0 +1,36 @@
+# 변수의 선언
+var = 42
+
+# print(var) # 42
+
+# print(var + 10)
+# print(var - 5)
+# print(var / 10)
+# print(var // 10)
+# print(var % 10)
+
+var2 = var / 10
+
+# print ( var > var2)
+
+# 실수형 변수
+var_float = 3.14
+# print(3.14 * 6)
+# print(3.14 / 2)
+
+# result = var * 10 + 5 # 425
+# print(result)
+# result = 5 + var * 10 # 425
+# print(result)
+# result = (5 + var) * 10 # 470
+# print(result)
+
+is_true = True
+is_false = False
+print(is_true and is_true) # True
+print(is_true and is_false) # False
+print(is_false and is_false) # False
+
+print(is_true or is_true) # True
+print(is_true or is_false) # True
+print(is_false or is_false) # False
