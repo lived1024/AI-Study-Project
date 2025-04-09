4609bbb618c523f4439c15c6d190f21ee8f2c99b
lived1024 <lived1024@gmail.com>
2025-04-09 17:52:55 +0900

    파이썬 조건문 실습

--- Python 기본/흐름제어/control_flow.py
+++ Python 기본/흐름제어/control_flow.py
@@ -19,3 +19,37 @@ elif condition is "흐림" :
   print("날씨가 많이 흐립니다.")
 elif condition is "맑음" and rain_rate >= 0.7 :
   print("맑지만 비가 올 확률이 높습니다.")
+
+
+# 사용자로부터 두 개의 값을 입력받고, 첫번째가 크면 Win, 두번째가 크면 Lose, 같으면 Draw
+
+value1 = input("첫 번째 값을 입력하세요.")
+value2 = input("두 번째 값을 입력하세요.")
+
+num_var1 = int(value1)
+num_var2 = int(value2)
+
+if num_var1 > num_var2 :
+  print("Win")
+elif num_var1 < num_var2 :
+  print("Lose")
+else :
+  print("Draw")
+
+
+# 시험점수 입력받아 성적 출력 (A: 90~99, B: 80~89, C: 70~79, D: 60~69, F: ~59
+score = int(input("시험 성적을 입력하세요."))
+if score >= 90 and score < 100 :
+  grade = "A"
+elif score >= 80 and score < 90 :
+  grade = "B"
+elif score >= 70 and score < 80 :
+  grade = "C"
+elif score >= 60 and score < 70 :
+  grade = "D"
+elif score >= 1 and score < 60 :
+  grade = "F"
+else :
+  grade = "ERROR"
+
+if grade != "ERROR" : print(f"성적은 {grade}입니다")
