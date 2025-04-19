d9624910ab3be047722362ea154cbb272c37911d
lived1024 <lived1024@gmail.com>
2025-04-19 23:12:26 +0900

    파이썬 모듈 실습

---
+++ Python 기본/모듈, 패키지/실습/module/main_module.py
@@ -0,0 +1,8 @@
+import math_module
+
+result_add = math_module.add(30, 7)
+result_sub = math_module.sub(30, 7)
+result_mul = math_module.mul(30, 7)
+result_div = math_module.div(30, 7)
+
+print(f"math_module 사칙연산 : {result_add}  /  {result_sub}  /  {result_mul}  /  {result_div}")
