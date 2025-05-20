b571d0563e9304efcbdd5a4fb6c73613cfcdc6cb
lived1024 <lived1024@gmail.com>
2025-05-20 18:59:33 +0900

    feat: YouTube 자막 처리 기능 추가
    - YouTube 자막 로딩 시 한국어 자막 지정 옵션 추가

--- Part 3/Part 3-6. Langchain Youtube.py
+++ Part 3/Part 3-6. Langchain Youtube.py
@@ -7,9 +7,50 @@ result = tool.run("IT 핫이슈")
 print(result)
 
 # pip install --upgrade --quiet youtube-transcript-api pytube faiss-cpu tiktoken langchain langchain_openai
-from langchain.document_loaders import YoutubeLoader  # 유튜브 로더
-from langchain.embeddings import OpenAIEmbeddings     # 임베딩 모델
-from langchain.vectorstores import FAISS              # 벡터 저장소
+from langchain_community.document_loaders import YoutubeLoader  # 유튜브 로더
+from langchain_openai import OpenAIEmbeddings     # 임베딩 모델
+from langchain_community.vectorstores import FAISS              # 벡터 저장소
 from langchain.chains import RetrievalQA              # 질문 답변 툴
 from langchain_openai import ChatOpenAI               # OpenAI 모델
 from pprint import pprint                             # 아웃풋 프린트 툴
+import os, openai
+
+# API_KEY.txt 파일에서 API 키 읽기
+# API_KEY.txt 파일은 현재 작업 디렉토리에 있어야 합니다.
+# 이 코드는 현재 작업 디렉토리로 이동하여 API_KEY.txt 파일을 읽습니다.
+os.chdir(os.path.dirname(__file__))
+with open('API_KEY.txt', 'r') as file:
+    os.environ["OPENAI_API_KEY"] = file.read().strip()
+    
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+# 임베딩하는 툴 생성
+embeddings = OpenAIEmbeddings()
+
+# 유튜브 링크에서 자막 내용 가지고 오기
+loader = YoutubeLoader.from_youtube_url(
+    "https://youtu.be/w8xG4Dzl7p0?si=63XXWtrRVSLvvjDS",
+    language=['ko']  # 한국어 자막 사용 - 영어인 경우 생략 가능
+)
+documents = loader.load()
+
+# 벡터 저장소
+db = FAISS.from_documents(documents, embeddings)
+retriever = db.as_retriever()
+
+# 사용할 언어모델
+llm = ChatOpenAI(model="gpt-4")
+
+# RetrievalQA를 써서 "가져오는 기계" 만들기
+qa = RetrievalQA.from_chain_type(
+    llm=llm,
+    # chain_type="stuff",
+    retriever=retriever,
+    return_source_documents=True
+)
+
+# 질문 정의하고 물어보기
+query = "무슨 내용인지 요약해줘"
+result = qa.invoke({"query": query})
+
+pprint(result['result'])
