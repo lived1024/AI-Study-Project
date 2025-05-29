d82df976e9495518790bd0e3cd333798eb8a0d4b
lived1024 <lived1024@gmail.com>
2025-05-29 21:22:54 +0900

    refactor: Rename embedding function and add similarity calculations
    - Renamed the `make_embedding` function to `get_embedding` for clarity.
    - Introduced a new `cosine_similarity` function to compute similarity between embeddings.
    - Added `get_similarity` function to evaluate and print similarity scores for given text pairs.
    - Updated embedding calls to use the new function name.

--- Part 4/Embedding.py
+++ Part 4/Embedding.py
@@ -8,7 +8,7 @@ import numpy as np
 # API 키 로드 및 클라이언트 생성
 client = load_openai_api_key()
 
-def make_embedding(text):
+def get_embedding(text):
   
   return np.array(client.embeddings.create(
     model="text-embedding-ada-002",
@@ -16,8 +16,8 @@ def make_embedding(text):
     encoding_format="float"
   ).data[0].embedding)
 
-kids_embedding = make_embedding("어린아이")
-children_embedding = make_embedding("어린이")
+kids_embedding = get_embedding("어린아이")
+children_embedding = get_embedding("어린이")
 
 print(len(kids_embedding))
 print('----------------------------------------------------')
@@ -26,4 +26,26 @@ print(len(children_embedding))
 print(type(kids_embedding)) # numpy.ndarray
 
 
-# 유사도 비교
+# 유사도 비교 - 두 벡터가 유사한 경우 1, 유사도가 낮을수록 -1
+def cosine_similarity(vector_a, vector_b):
+  # Compute the dot product between the two vectors
+  dot_product = np.dot(vector_a, vector_b)
+  # Compute the norm(magnitude) of each vector
+  norm_a = np.linalg.norm(vector_a)
+  norm_b = np.linalg.norm(vector_b)
+  # Compute the cosine similarity
+  cosine_similarity = dot_product / (norm_a * norm_b)
+  return cosine_similarity
+
+# result = cosine_similarity(get_embedding("어린아이"), get_embedding("어린이"))      # 0.964651954216103
+result = cosine_similarity(get_embedding("어린아이"), get_embedding("어린아이"))      # 1
+print("Cosine Similarity: ", result)
+
+
+def get_similarity(text_a, text_b):
+  result = round(cosine_similarity(get_embedding(text_a), get_embedding(text_b)), 2)
+  print(f"얼마나 가깝나요? {result}")
+
+get_similarity("학교", "school")      # 0.86
+get_similarity("wifi", "학원")        # 0.77
+get_similarity("비행기", "항공기")     # 0.93
